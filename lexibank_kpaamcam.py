5c47cd8b9de8eb2cf6b450500ec8605a73f77a58
Robert Forkel <xrotwang@googlemail.com>
2021-06-22 13:45:42 +0200

    Streamlined CLDF creation
    We don't go through lingpy.Wordlist anymore, but transform
    parallel_lists.csv directly.

--- lexibank_kpaamcam.py
+++ lexibank_kpaamcam.py
@@ -1,30 +1,37 @@
-from pathlib import Path
+import pathlib
+import collections
+
 from clldutils.misc import slug
-from pylexibank import FormSpec, Lexeme, Concept, Language
+from pylexibank import FormSpec
 from pylexibank import Dataset as BaseDataset
-from pylexibank import progressbar
-from lingpy import *
-import attr
 
-@attr.s
-class CustomConcept(Concept):
-    PartOfSpeech = attr.ib(default=None)
-
-@attr.s
-class CustomLexeme(Lexeme):
-    Reflex_ID = attr.ib(default=None)
-
-@attr.s
-class CustomLanguage(Language):
-    SubGroup = attr.ib(default=None)
+speakerInfos = collections.OrderedDict([
+    [	"NVB-Abar-7", "abar1239"	],
+    [	"ECL-Abar-8", "abar1239"	],
+    [	"NEM-Ajumbu-9", "mbuu1238"	],
+    [	"KDC-Ajumbu-10", "mbuu1238"	],
+    [	"ENB-BIYA-1", "biya1235"	],
+    [	"ICN-BIYA-2", "biya1235"	],
+    [	"NNB-Buu-3", "buuu1246"	],
+    [	"MNJ-Buu-4", "buuu1246"	],
+    [	"KHK-FANG-12", "fang1248"	],
+    [	"DPN-FANG-13", "fang1248"	],
+    [	"JGY-Koshin-3", "kosh1246"	],
+    [	"TEL-Koshin-4", "kosh1246"	],
+    [	"KCS-Kung-3", "kung1260"	],
+    [	"NJS-Kung-4", "kung1260"	],
+    [	"NMN-Mundabli-3", "mund1340"	],
+    [	"CEN-Mundabli-2", "mund1340"	],
+    [	"NGT-Munken-3", "munk1244"	],
+    [	"NUN-Munken-4", "munk1244"	],
+    [	"MCA-Ngun-3", "ngun1279"	],
+    [	"KBM-Ngun-4", "ngun1279"	],
+])
 
 
 class Dataset(BaseDataset):
-    dir = Path(__file__).parent
+    dir = pathlib.Path(__file__).parent
     id = "LowerFungomIndividualWordlists"
-    lexeme_class = CustomLexeme
-    language_class = CustomLanguage
-    concept_class = CustomConcept
 
     # define the way in which forms should be handled
     form_spec = FormSpec(
@@ -41,33 +48,26 @@ class Dataset(BaseDataset):
         """
         Convert the raw data to a CLDF dataset.
         """
-
-        # Write source
         args.writer.add_sources()
 
-        # Write languages
-        languages = args.writer.add_languages(lookup_factory='Name')
-
-        # Write concepts
         concepts = {}
         for concept in self.concepts:
             idx = concept['NUMBER']+'_'+slug(concept['ENGLISH'])
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept['ENGLISH'],
-                    PartOfSpeech=concept['POS'],
-                    )
+                ID=idx,
+                Name=concept['ENGLISH'],
+            )
             concepts[concept['ENGLISH']] = idx
 
-        # Write forms
-        wl = Wordlist(self.raw_dir.joinpath('oneEntryPerRow-wordlist.tsv').as_posix())
-        for idx in progressbar(wl):
-            #print(languages[wl[idx, 'doculect']])
-            args.writer.add_forms_from_value(
-                    Value=wl[idx, 'value'],
-                    Language_ID=languages[wl[idx, 'doculect']],
-                    Parameter_ID=concepts[wl[idx, 'concept']],
-                    Reflex_ID=wl[idx, 'reflex_id'],
+        for i, entry in enumerate(self.raw_dir.read_csv('parallel_lists.csv', dicts=True)):
+            for lid, glottocode in speakerInfos.items():
+                if i == 0:
+                    args.writer.add_language(ID=lid, Name=lid, Glottocode=glottocode)
+                if not entry[lid].strip():
+                    continue
+                args.writer.add_forms_from_value(
+                    Value=entry[lid],
+                    Language_ID=lid,
+                    Parameter_ID=concepts[entry['Concept']],
                     Source=[]
-                    )
-
+                )
